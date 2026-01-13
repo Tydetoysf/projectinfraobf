c44954a7b5c271c5af04262c707f5ea5c6f0fb35
Tydetoysf <161236899+Tydetoysf@users.noreply.github.com>
2026-01-13 17:53:53 -0600

    Update script URL in arsenal.lua

--- arsenal.lua
+++ arsenal.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Tydetoysf/projectinfra.com/main/scripts/infra.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Tydetoysf/test/main/arsenal"))()
